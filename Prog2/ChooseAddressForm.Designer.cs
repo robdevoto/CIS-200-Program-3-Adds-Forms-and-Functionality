24fb3cefaf5cc5ef61368ac22547a247539d6191
robde <robde@LAPTOP-KM82B2SA>
2023-04-07 16:59:33 -0400

    Add project files.

---
+++ Prog2/ChooseAddressForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace UPVApp
+{
+    partial class ChooseAddressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.addressListLbl = new System.Windows.Forms.Label();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.addressListCbo = new System.Windows.Forms.ComboBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addressListLbl
+            // 
+            this.addressListLbl.AutoSize = true;
+            this.addressListLbl.Location = new System.Drawing.Point(72, 30);
+            this.addressListLbl.Name = "addressListLbl";
+            this.addressListLbl.Size = new System.Drawing.Size(151, 16);
+            this.addressListLbl.TabIndex = 0;
+            this.addressListLbl.Text = "Choose Address to Edit:";
+            // 
+            // okBtn
+            // 
+            this.okBtn.Location = new System.Drawing.Point(35, 105);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(83, 33);
+            this.okBtn.TabIndex = 1;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(181, 105);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(79, 33);
+            this.cancelBtn.TabIndex = 2;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cancelBtn_MouseDown);
+            // 
+            // addressListCbo
+            // 
+            this.addressListCbo.FormattingEnabled = true;
+            this.addressListCbo.Location = new System.Drawing.Point(75, 62);
+            this.addressListCbo.Name = "addressListCbo";
+            this.addressListCbo.Size = new System.Drawing.Size(148, 24);
+            this.addressListCbo.TabIndex = 3;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ChooseAddressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 162);
+            this.Controls.Add(this.addressListCbo);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.addressListLbl);
+            this.Name = "ChooseAddressForm";
+            this.Text = "Choose Address Form";
+            this.Load += new System.EventHandler(this.ChooseAddressForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label addressListLbl;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.ComboBox addressListCbo;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Prog2/ChooseAddressForm.cs
@@ -0,0 +1,102 @@
+﻿// Program 3
+// CIS 200-75
+// Fall 2022
+// Due: 11/23/2022
+// By: 1001001
+
+// File: ChooseAddressForm.cs
+// This class creates the GUI for Program 3 for editing existing addresses.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPVApp
+{
+    public partial class ChooseAddressForm : Form
+    {
+        private List<Address> addressList;  // List of addresses used to fill combo boxes
+
+        // Precondition:  None
+        // Postcondition: The forms GUI is prepared for display
+        public ChooseAddressForm(List<Address> addresses)
+        {
+            InitializeComponent();
+            addressList = addresses;
+        }
+
+        //Property
+        public int AddressIndex
+        {
+            // Precondition:  An item from the combo box has been selected
+            // Postcondition: The index of the selected value is returned
+            get
+            {
+                return addressListCbo.SelectedIndex;
+            }
+
+            // Precondition:  value is between -1 (inclusive) and the addressList.Count
+            // Postcondition: The specified index is set
+            set
+            {
+                if ((value >= -1) && (value < addressList.Count))
+                    addressListCbo.SelectedIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException("AddressIndex", value,
+                        "Index must be valid");
+            }
+        }
+
+        // Precondition:  None
+        // Postcondition: The address list is used to populate the name in the combo box
+        private void ChooseAddressForm_Load(object sender, EventArgs e)
+        {
+            foreach (Address a in addressList)
+            {
+                addressListCbo.Items.Add(a.Name);
+            }
+
+            addressListCbo.SelectedIndex = 0;
+        }
+
+        // Precondition:  The cancel button is selected
+        // Postcondition: The form is closed
+        private void cancelBtn_MouseDown(object sender, MouseEventArgs e)
+        {
+            DialogResult= DialogResult.Cancel;
+        }
+
+        // Precondition:  Focus is shifting from the combo box
+        // Postcondition: An error provider shows that no address is selected
+        private void addressListCbo_Validating(object sender, CancelEventArgs e)
+        {
+            if (addressListCbo.SelectedIndex == -1)
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(addressListCbo, "Please select an address.");
+            }
+        }
+
+        // Precondition:  The data is okay and has passed validating
+        // Postcondition: Error provider cleared and focus allowed to change
+        private void addressListCbo_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(addressListCbo, "");
+        }
+
+        // Precondition:  The ok button is clicked
+        // Postcondition: If everthing validated then form will close,
+        //                otherwise form stays open for correction
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren())
+                DialogResult= DialogResult.OK;
+        }
+    }
+}
